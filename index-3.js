81d2eb264b6ce68840b8ed417c632c4475992226
Ekshin683 <ekshindeep@gmail.com>
2026-02-19 11:33:55 +0530

    update

--- index-3.js
+++ index-3.js
@@ -3,7 +3,10 @@ const app = express();
 app.listen(8080, () => {
     console.log("Server started")
 });
-
+app.get("/",(req,res) => {
+    console.log(req.url)
+    res.send("response from server for /")
+})
 app.get("/home",(req,res) => {
     console.log(req.url);
     res.send("response from server for /home")
