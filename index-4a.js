b2ab9e63df6165dc9a2175ca0e2ffdbffe558154
Ekshin683 <ekshindeep@gmail.com>
2026-02-19 13:44:40 +0530

    update

--- index-4a.js
+++ index-4a.js
@@ -8,3 +8,8 @@ app.get("/:id1/:id2", (req,res) => {
     const sum = parseInt(req.params.id1) + parseInt(req.params.id2);
     res.send(sum);
 })
+
+app.get("/x/:id1/y/:id2/z/:id3", (req,res) => {
+    const sum = parseInt(req.params.id1) + parseInt(req.params.id2) + parseInt(req.params.id3);
+    res.send(sum);
+})
