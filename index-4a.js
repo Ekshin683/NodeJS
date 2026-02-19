9112ce40e563d0ae2dfb8ff551eba04e7f6e2a7d
Ekshin683 <ekshindeep@gmail.com>
2026-02-19 13:55:13 +0530

    update

--- index-4a.js
+++ index-4a.js
@@ -4,12 +4,16 @@ app.listen(8080, () => {
     console.log("Server Started");
 })
 
-app.get("/:id1/:id2", (req,res) => {
-    const sum = parseInt(req.params.id1) + parseInt(req.params.id2);
+app.get("/:a/:b", (req,res) => {
+    const sum = parseInt(req.params.a) + parseInt(req.params.b);
     res.send(sum);
 })
 
-app.get("/x/:id1/y/:id2/z/:id3", (req,res) => {
-    const sum = parseInt(req.params.id1) + parseInt(req.params.id2) + parseInt(req.params.id3);
+app.get("/x/:a/y/:b/z/:c", (req,res) => {
+    const sum = parseInt(req.params.a) + parseInt(req.params.b) + parseInt(req.params.c);
     res.send(sum);
+})
+
+app.get("/:id/:id1/:id2/:id3", (req,res) => {
+    res.send("Hello Students!")
 })
