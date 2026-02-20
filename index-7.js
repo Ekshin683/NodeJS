42cbbcf19a790f017593e70728dd57853130fe24
Ekshin683 <ekshindeep@gmail.com>
2026-02-20 09:15:29 +0530

    update

--- index-7.js
+++ index-7.js
@@ -3,12 +3,42 @@ const app = express();
 app.listen(8080,()=>{
     console.log("Server started")
 })
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    const user = {
+let users = [
+    {
         name:"Ekshin",
-        email:"ekshindeep@gmail.com",
+        email:"ekshin@gmail.com",
+        role:"student"
+    },
+    {
+        name:"Ajay",
+        email:"ajay@gmail.com",
+        role:"student"
+    },
+    {
+        name:"Sumanth",
+        email:"sumanth@gmail.com",
         role:"student"
     }
-    res.send(user);
+]
+app.get("/", (req,res) => {
+    res.send(users);
+})
+
+// app.get("/:idx",(req,res) => {
+    
+// })
+
+app.post("/",(req,res) => {
+    // console.log(req.body);
+    const user = req.body;
+    users.push(user);
+    res.json(users);
+})
+
+app.delete("/:idx",(req,res) => {
+    const index = users.findIndex((user) => user.id === parseInt(req.params.idx));
+    const removed = users.splice(index, 1)[0];
+    res.json(users);
 })
